924223d1f1fc15e82b1171f2c202fcdba4fcdc4b
Tony B <aaborel@gmail.com>
2018-12-14 12:45:41 -0700

    Fixed prime number checker

--- Numbers/prime number checker.js
+++ Numbers/prime number checker.js
@@ -1,15 +1,22 @@
 // Takes one number and returns True or False depending on if it is Prime or not
-// Partially working. Line 12 always runs. Need to get it to only output for a prime.
+// This function also tells you all the divisors for a number.
 
 function isPrimeNumber(num) {
-    let p = 2
+
+    let p = 2;
+    let count = 0;
+    
     while (p < num) {
         if (num % p === 0) {
-            console.log('false, divisible by', p);
-            } 
+                count++;
+                console.log('false, divisible by', p);
+            }
         p++
     }
-    console.log('true prime number')
+
+    if (count === 0 ) {
+        console.log('true, prime number')
+    }
 }
 
-isPrimeNumber(8);
+isPrimeNumber(2255);
